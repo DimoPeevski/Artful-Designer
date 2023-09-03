c626b6eb4212b3182c181447c3888c63cc98c849
Dimo Peevski <didiprophet@gmail.com>
2023-09-03 16:43:20 +0300

    Final design

--- Artful Designer/ArtfulDesigner.Designer.cs
+++ Artful Designer/ArtfulDesigner.Designer.cs
@@ -30,6 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArtfulDesigner));
             labelWelcome = new Label();
+            labelBox1 = new Label();
+            labelBox2 = new Label();
+            labelBox3 = new Label();
+            labelBox4 = new Label();
+            buttonBackBox1 = new Button();
+            buttonLetsGo = new Button();
+            buttonNextBox1 = new Button();
+            buttonBackBox2 = new Button();
+            buttonBackBox3 = new Button();
+            buttonBackBox4 = new Button();
+            buttonNextBox2 = new Button();
+            buttonNextBox3 = new Button();
+            buttonNextBox4 = new Button();
+            buttonStartOver = new Button();
             SuspendLayout();
             // 
             // labelWelcome
@@ -42,11 +56,149 @@
             labelWelcome.Name = "labelWelcome";
             labelWelcome.UseCompatibleTextRendering = true;
             // 
+            // labelBox1
+            // 
+            resources.ApplyResources(labelBox1, "labelBox1");
+            labelBox1.BackColor = Color.LightCyan;
+            labelBox1.Name = "labelBox1";
+            // 
+            // labelBox2
+            // 
+            resources.ApplyResources(labelBox2, "labelBox2");
+            labelBox2.BackColor = SystemColors.ButtonHighlight;
+            labelBox2.Name = "labelBox2";
+            // 
+            // labelBox3
+            // 
+            resources.ApplyResources(labelBox3, "labelBox3");
+            labelBox3.BackColor = SystemColors.ButtonHighlight;
+            labelBox3.Name = "labelBox3";
+            // 
+            // labelBox4
+            // 
+            resources.ApplyResources(labelBox4, "labelBox4");
+            labelBox4.BackColor = SystemColors.ButtonHighlight;
+            labelBox4.Name = "labelBox4";
+            // 
+            // buttonBackBox1
+            // 
+            resources.ApplyResources(buttonBackBox1, "buttonBackBox1");
+            buttonBackBox1.BackColor = Color.DarkCyan;
+            buttonBackBox1.FlatAppearance.BorderSize = 0;
+            buttonBackBox1.ForeColor = SystemColors.ButtonHighlight;
+            buttonBackBox1.Name = "buttonBackBox1";
+            buttonBackBox1.UseVisualStyleBackColor = false;
+            buttonBackBox1.Click += ButtonBackBox1_Click;
+            // 
+            // buttonLetsGo
+            // 
+            resources.ApplyResources(buttonLetsGo, "buttonLetsGo");
+            buttonLetsGo.BackColor = Color.DarkCyan;
+            buttonLetsGo.FlatAppearance.BorderSize = 0;
+            buttonLetsGo.ForeColor = SystemColors.ButtonHighlight;
+            buttonLetsGo.Name = "buttonLetsGo";
+            buttonLetsGo.UseVisualStyleBackColor = false;
+            buttonLetsGo.Click += buttonLetsGo_Click;
+            // 
+            // buttonNextBox1
+            // 
+            resources.ApplyResources(buttonNextBox1, "buttonNextBox1");
+            buttonNextBox1.BackColor = Color.DarkCyan;
+            buttonNextBox1.FlatAppearance.BorderSize = 0;
+            buttonNextBox1.ForeColor = SystemColors.ButtonHighlight;
+            buttonNextBox1.Name = "buttonNextBox1";
+            buttonNextBox1.UseVisualStyleBackColor = false;
+            buttonNextBox1.Click += ButtonNextBox1_Click;
+            // 
+            // buttonBackBox2
+            // 
+            resources.ApplyResources(buttonBackBox2, "buttonBackBox2");
+            buttonBackBox2.BackColor = Color.DarkCyan;
+            buttonBackBox2.FlatAppearance.BorderSize = 0;
+            buttonBackBox2.ForeColor = SystemColors.ButtonHighlight;
+            buttonBackBox2.Name = "buttonBackBox2";
+            buttonBackBox2.UseVisualStyleBackColor = false;
+            buttonBackBox2.Click += ButtonBackBox2_Click;
+            // 
+            // buttonBackBox3
+            // 
+            resources.ApplyResources(buttonBackBox3, "buttonBackBox3");
+            buttonBackBox3.BackColor = Color.DarkCyan;
+            buttonBackBox3.FlatAppearance.BorderSize = 0;
+            buttonBackBox3.ForeColor = SystemColors.ButtonHighlight;
+            buttonBackBox3.Name = "buttonBackBox3";
+            buttonBackBox3.UseVisualStyleBackColor = false;
+            buttonBackBox3.Click += ButtonBackBox3_Click;
+            // 
+            // buttonBackBox4
+            // 
+            resources.ApplyResources(buttonBackBox4, "buttonBackBox4");
+            buttonBackBox4.BackColor = Color.DarkCyan;
+            buttonBackBox4.FlatAppearance.BorderSize = 0;
+            buttonBackBox4.ForeColor = SystemColors.ButtonHighlight;
+            buttonBackBox4.Name = "buttonBackBox4";
+            buttonBackBox4.UseVisualStyleBackColor = false;
+            buttonBackBox4.Click += ButtonBackBox4_Click;
+            // 
+            // buttonNextBox2
+            // 
+            resources.ApplyResources(buttonNextBox2, "buttonNextBox2");
+            buttonNextBox2.BackColor = Color.DarkCyan;
+            buttonNextBox2.FlatAppearance.BorderSize = 0;
+            buttonNextBox2.ForeColor = SystemColors.ButtonHighlight;
+            buttonNextBox2.Name = "buttonNextBox2";
+            buttonNextBox2.UseVisualStyleBackColor = false;
+            buttonNextBox2.Click += ButtonNextBox2_Click;
+            // 
+            // buttonNextBox3
+            // 
+            resources.ApplyResources(buttonNextBox3, "buttonNextBox3");
+            buttonNextBox3.BackColor = Color.DarkCyan;
+            buttonNextBox3.FlatAppearance.BorderSize = 0;
+            buttonNextBox3.ForeColor = SystemColors.ButtonHighlight;
+            buttonNextBox3.Name = "buttonNextBox3";
+            buttonNextBox3.UseVisualStyleBackColor = false;
+            buttonNextBox3.Click += ButtonNextBox3_Click;
+            // 
+            // buttonNextBox4
+            // 
+            resources.ApplyResources(buttonNextBox4, "buttonNextBox4");
+            buttonNextBox4.BackColor = Color.DarkCyan;
+            buttonNextBox4.FlatAppearance.BorderSize = 0;
+            buttonNextBox4.ForeColor = SystemColors.ButtonHighlight;
+            buttonNextBox4.Name = "buttonNextBox4";
+            buttonNextBox4.UseVisualStyleBackColor = false;
+            buttonNextBox4.Click += ButtonNextBox4_Click;
+            // 
+            // buttonStartOver
+            // 
+            resources.ApplyResources(buttonStartOver, "buttonStartOver");
+            buttonStartOver.BackColor = Color.DarkCyan;
+            buttonStartOver.FlatAppearance.BorderSize = 0;
+            buttonStartOver.ForeColor = SystemColors.ButtonHighlight;
+            buttonStartOver.Name = "buttonStartOver";
+            buttonStartOver.UseVisualStyleBackColor = false;
+            buttonStartOver.Click += buttonStartOver_Click;
+            // 
             // ArtfulDesigner
             // 
             resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.artful_designer_background;
+            Controls.Add(buttonStartOver);
+            Controls.Add(buttonNextBox4);
+            Controls.Add(buttonNextBox3);
+            Controls.Add(buttonNextBox2);
+            Controls.Add(buttonBackBox4);
+            Controls.Add(buttonBackBox3);
+            Controls.Add(buttonBackBox2);
+            Controls.Add(buttonNextBox1);
+            Controls.Add(buttonLetsGo);
+            Controls.Add(buttonBackBox1);
+            Controls.Add(labelBox4);
+            Controls.Add(labelBox3);
+            Controls.Add(labelBox2);
+            Controls.Add(labelBox1);
             Controls.Add(labelWelcome);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.FixedSingle;
@@ -59,5 +211,19 @@
         #endregion
 
         public Label labelWelcome;
+        private Label labelBox1;
+        private Label labelBox2;
+        private Label labelBox3;
+        private Label labelBox4;
+        private Button buttonBackBox1;
+        private Button buttonLetsGo;
+        private Button buttonNextBox1;
+        private Button buttonBackBox2;
+        private Button buttonBackBox3;
+        private Button buttonBackBox4;
+        private Button buttonNextBox2;
+        private Button buttonNextBox3;
+        private Button buttonNextBox4;
+        private Button buttonStartOver;
     }
 }
--- Artful Designer/ArtfulDesigner.cs
+++ Artful Designer/ArtfulDesigner.cs
@@ -9,7 +9,182 @@ namespace Artful_Designer
 
         private void ArtfulDesigner_Load(object sender, EventArgs e)
         {
+            labelBox1.BackColor = Color.LightCyan;
+            labelBox2.BackColor = SystemColors.ButtonHighlight;
+            labelBox3.BackColor = SystemColors.ButtonHighlight;
+            labelBox2.BackColor = SystemColors.ButtonHighlight;
 
+            labelBox1.Visible = false;
+            labelBox2.Visible = false;
+            labelBox3.Visible = false;
+            labelBox4.Visible = false;
+
+            buttonNextBox1.Visible = false;
+            buttonNextBox2.Visible = false;
+            buttonNextBox3.Visible = false;
+            buttonNextBox4.Visible = false;
+
+            buttonBackBox1.Visible = false;
+            buttonBackBox2.Visible = false;
+            buttonBackBox3.Visible = false;
+            buttonBackBox4.Visible = false;
+
+            buttonStartOver.Visible = false;
+        }
+
+        private void buttonLetsGo_Click(object sender, EventArgs e)
+        {
+            labelBox1.Visible = true;
+            labelBox2.Visible = true;
+            labelBox3.Visible = true;
+            labelBox4.Visible = true;
+
+            buttonLetsGo.Visible = false;
+
+            buttonNextBox1.Visible = true;
+            buttonBackBox1.Visible = true;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 1 \r\n\r\n   ";
+        }
+
+        private void ButtonBackBox1_Click(object sender, EventArgs e)
+        {
+            labelBox1.Visible = false;
+            labelBox2.Visible = false;
+            labelBox3.Visible = false;
+            labelBox4.Visible = false;
+
+            buttonLetsGo.Visible = true;
+            buttonNextBox1.Visible = false;
+            buttonBackBox1.Visible = false;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   Hello, I'm Arti! \r\n\r\n   I'm here to welcome you to our humble application, which allows you to create an unlimited number\r\n   of images for your projects or social media by following a predefined text file. \r\n\r\n   From it, we will create what's expected - beautiful images that will align with your design vision. We'll walk \r\n   through the program in four steps, and during each one, you'll need to select specific settings.\r\n\r\n   On the final step, after clicking the 'Generate' button, you'll be able to save the files you've created. \r\n\r\n\r\n   We look forward to having fun together. \r\n\r\n   Good luck!\r\n";
+        }
+
+        private void ButtonBackBox2_Click(object sender, EventArgs e)
+        {
+            buttonNextBox1.Visible = true;
+            buttonNextBox2.Visible = false;
+
+            buttonBackBox1.Visible = true;
+            buttonBackBox2.Visible = false;
+
+            labelBox1.BackColor = Color.LightCyan;
+            labelBox2.BackColor = SystemColors.ButtonHighlight;
+
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 1 \r\n\r\n   ";
+        }
+
+        private void ButtonBackBox3_Click(object sender, EventArgs e)
+        {
+            buttonNextBox2.Visible = true;
+            buttonNextBox3.Visible = false;
+
+            buttonBackBox2.Visible = true;
+            buttonBackBox3.Visible = false;
+
+            labelBox2.BackColor = Color.LightCyan;
+            labelBox3.BackColor = SystemColors.ButtonHighlight;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 2 \r\n\r\n   ";
+        }
+
+        private void ButtonBackBox4_Click(object sender, EventArgs e)
+        {
+            buttonNextBox3.Visible = true;
+            buttonNextBox4.Visible = false;
+
+            buttonBackBox3.Visible = true;
+            buttonBackBox4.Visible = false;
+
+            labelBox3.BackColor = Color.LightCyan;
+            labelBox4.BackColor = SystemColors.ButtonHighlight;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 3 \r\n\r\n   ";
+        }
+
+        private void ButtonNextBox1_Click(object sender, EventArgs e)
+        {
+            buttonNextBox1.Visible = false;
+            buttonNextBox2.Visible = true;
+
+            buttonBackBox1.Visible = false;
+            buttonBackBox2.Visible = true;
+
+            labelBox1.BackColor = Color.Gainsboro;
+            labelBox2.BackColor = Color.LightCyan;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 2 \r\n\r\n   ";
+        }
+
+        private void ButtonNextBox2_Click(object sender, EventArgs e)
+        {
+            buttonNextBox2.Visible = false;
+            buttonNextBox3.Visible = true;
+
+            buttonBackBox2.Visible = false;
+            buttonBackBox3.Visible = true;
+
+            labelBox2.BackColor = Color.Gainsboro;
+            labelBox3.BackColor = Color.LightCyan;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 3 \r\n\r\n   ";
+        }
+
+        private void ButtonNextBox3_Click(object sender, EventArgs e)
+        {
+            buttonNextBox3.Visible = false;
+            buttonNextBox4.Visible = true;
+
+            buttonBackBox3.Visible = false;
+            buttonBackBox4.Visible = true;
+
+            labelBox3.BackColor = Color.Gainsboro;
+            labelBox4.BackColor = Color.LightCyan;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   This is BOX 4 \r\n\r\n   ";
+        }
+
+        private void ButtonNextBox4_Click(object sender, EventArgs e)
+        {
+            labelBox1.Visible = false;
+            labelBox2.Visible = false;
+            labelBox3.Visible = false;
+            labelBox4.Visible = false;
+
+            buttonNextBox1.Visible = false;
+            buttonNextBox2.Visible = false;
+            buttonNextBox3.Visible = false;
+            buttonNextBox4.Visible = false;
+
+            buttonBackBox1.Visible = false;
+            buttonBackBox2.Visible = false;
+            buttonBackBox3.Visible = false;
+            buttonBackBox4.Visible = false;
+
+            buttonStartOver.Visible = true;
+
+            labelWelcome.Height = 185;
+
+            labelWelcome.Text = "\r\n\r\n   \r\n\r\n\r\n   Your files have been generated successfully. You can locate them in the specified folder. \r\n\r\n   ";
+
+            //generate logic here
+        }
+
+        private void buttonStartOver_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the current form
+            ArtfulDesigner newLoad = new ArtfulDesigner();
+
+            // Hide the current form
+            this.Hide();
+
+            // Show the new form
+            newLoad.ShowDialog();
+
+            // Close the current form
+            this.Close();
         }
     }
 }
